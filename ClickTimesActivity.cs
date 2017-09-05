93d5a50eca06e9056222e423e62892c5fe9e10b7
Alex Xu <764365184@qq.com>
2017-09-05 17:09:42 +0800

    Count click times

---
+++ ClickTimesActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App2
+{
+    [Activity(Label = "ClickTimesActivity")]
+    public class ClickTimesActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.ClickTimes);
+
+            var backButton = FindViewById<Button>(Resource.Id.BackButton);
+            var label = FindViewById<TextView>(Resource.Id.ShowLabel);
+
+            int count = Intent.GetIntExtra("Click Times", 0);
+
+            label.Text = $"You clicked {count} times";
+
+            backButton.Click += delegate
+            {
+                var intent = new Intent(this, typeof(MainActivity));
+                intent.PutExtra("Click Times", count);
+                StartActivity(intent);
+            };
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Widget;
+using Android.OS;
+
+namespace App2
+{
+
+    [Activity(Label = "Click App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            if (bundle != null)
+            {
+                count = bundle.GetInt("count");
+            }
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button clickButton = FindViewById<Button>(Resource.Id.ClickButton);
+            Button showButton = FindViewById<Button>(Resource.Id.ShowButton);
+
+            //var label = FindViewById<TextView>(Resource.Id.textView1);
+            //label.Text = $"You clicked {count} times";
+
+            count = Intent.GetIntExtra("Click Times", 0);
+
+            clickButton.Click += delegate
+            {
+                count++;
+                //label.Text = $"You clicked {count} times";
+            };
+
+            showButton.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(ClickTimesActivity));
+                intent.PutExtra("Click Times", count);
+                StartActivity(intent);
+            };
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            outState.PutInt("count", count);
+            base.OnSaveInstanceState(outState);
+        }
+
+        /*protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+
+            count = savedInstanceState.GetInt("count");
+        }*/
+    }
+
+    
+}
